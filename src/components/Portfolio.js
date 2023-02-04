a199edc1444859d1d5fc9969c902a651d012f242
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-02-05 00:27:08 +0100

    Changed app icon

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import weatherapp from "../work/React-weather-app.jpeg"
 export default function Portfolio(){
     return (
         <div>
-            
+        <div>
+          <img src={weatherapp} alt="Weather App"  width={350} height={200}/> 
+          <h3>React Weather App</h3>
+          <small>Landing Page for React Weather Application</small>
+        </div>
         </div>
     )
 }
