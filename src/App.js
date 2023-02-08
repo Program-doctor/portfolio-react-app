6a7a01b283743865a8bfa31ef78e7d38148433e9
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-02-08 22:30:28 +0100

    Working on About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import About from './components/About';
+import About from './components/about/About';
 import Contact from './components/Contact';
 import Experience from './components/Experience';
 import Footer from './components/Footer';
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,13 @@
 import React from "react";
-import profile3 from "../profiles/profile1.jpg"
+import profile3 from "../../profiles/profile1.jpg";
+import "./About.css"
 
 export default function About(){
     return (
         <div id="about">
-            <p>Get to know</p>
+            <h5 className="text-light">Get to know</h5>
             <h2>About me</h2>
-            <div>
+            <div className="about__me">
                 <img src={profile3} width="250"
                 alt="me" />
             </div>
@@ -16,7 +17,7 @@ export default function About(){
             <p>1+ Year working</p></div>
             <div className="card"><h6>Projects</h6>
             <p>5+ Completed</p></div>
-            <p>Promise is a Frontend Developer, with skills in Responsive UI designs and Web development, who enjoys building products and applications that solve human problems.With a background in Customer Service, Tech Training & Support, Graphic Design, Content Writing, and Product Support. Promise is able to add value and build a dynamic career by developing scalable tech solutions that address specific user needs.</p>
+            <p className="text-light" >Promise is a Frontend Developer, with skills in Responsive UI designs and Web development, who enjoys building products and applications that solve human problems.With a background in Customer Service, Tech Training & Support, Graphic Design, Content Writing, and Product Support. Promise is able to add value and build a dynamic career by developing scalable tech solutions that address specific user needs.</p>
             <p>She is experienced in building and maintaining websites, implementing responsive designs, optimizing website performance and collaborating with any development team to implement new features and fix bugs.</p>
             <p>Promise is always looking for opportunities to learn and improve her skills in order to become a world-class Developer in the tech space. Her interests include Product Design, Product Development, Product Management, Data Science and Data Analysis.</p>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
