39c2185600b5e82833b062a28b49910e88a10a40
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-02-09 04:06:28 +0100

    Completed about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,26 +1,41 @@
 import React from "react";
 import profile3 from "../../profiles/profile1.jpg";
 import "./About.css"
+import { BiCodeBlock } from 'react-icons/bi';
+import { SlBadge } from 'react-icons/sl';
+import { AiFillFolderOpen } from 'react-icons/ai';
 
 export default function About(){
     return (
         <div id="about">
             <h5 className="text-light">Get to know</h5>
             <h2>About me</h2>
+            <div className="about-container">
             <div className="about__me">
                 <img src={profile3} width="250"
                 alt="me" />
             </div>
-            <div className="card"><h6>Industry</h6>
-            <p>Knowledge</p></div>
-            <div className="card"><h6>Experience</h6>
-            <p>1+ Year working</p></div>
-            <div className="card"><h6>Projects</h6>
-            <p>5+ Completed</p></div>
+            <div className="about-content">
+            <div className="about__cards-container">  
+            <div className="card">
+            <BiCodeBlock className="about-icon"/>
+            <h6>Industry</h6>
+            <small>Knowledge</small></div>
+            <div className="card">
+                <SlBadge className="about-icon"/>
+                <h6>Experience</h6>
+            <small>1+ Year working</small></div>
+            <div className="card">
+                <AiFillFolderOpen className="about-icon"/>
+                <h6>Projects</h6>
+            <small>5+ Completed</small></div>
+            </div>
             <p className="text-light" >Promise is a Frontend Developer, with skills in Responsive UI designs and Web development, who enjoys building products and applications that solve human problems.With a background in Customer Service, Tech Training & Support, Graphic Design, Content Writing, and Product Support. Promise is able to add value and build a dynamic career by developing scalable tech solutions that address specific user needs.</p>
             <p>She is experienced in building and maintaining websites, implementing responsive designs, optimizing website performance and collaborating with any development team to implement new features and fix bugs.</p>
             <p>Promise is always looking for opportunities to learn and improve her skills in order to become a world-class Developer in the tech space. Her interests include Product Design, Product Development, Product Management, Data Science and Data Analysis.</p>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+            </div>
+        </div>
         </div>
     )
 }
