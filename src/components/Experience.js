0de5c9c57b5ed27461570b6d8632233806687ec7
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-02-10 10:39:49 +0100

    Finished working on experience page

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "../components/Experience.css"
+import "../components/Experience.css";
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 export default function Experience(){
     return (
@@ -8,41 +9,139 @@ export default function Experience(){
         <div className="experiences">
         <div className="dev">
             <h2>Development & Design</h2>
-            <h3>HTML</h3>
-            <p>Experienced</p>
-            <h3>CSS</h3>
-            <p>Experienced</p>
-            <h3>JavaScript</h3>
-            <p>Intermediate</p>
-            <h3>Bootstrap</h3>
-            <p>Experienced</p>
-            <h3>React</h3>
-            <p>Intermediate</p>
-            <h3>Git & Github</h3>
-            <p>Experienced</p>
-            <h3>Figma</h3>
-            <p>Intermediate</p>
-            <h3>Ajax</h3>
-            <p>Experienced</p>
-            <h3>SEO management</h3>
-            <p>Intermediate</p>
-            <h3>Responsive Designs</h3>
-            <p>Experienced</p>
+            <div className="experience__content">
+                <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>HTML</h4>
+            <small className="text-light">Experienced</small>
             </div>
-        <div>
-            <h4>Soft Skills</h4>
-            <h6>Team Work</h6>
-            <p>Strong</p>
-            <h6>Problem Solving</h6>
-            <p>Good</p>
-            <h6>Creativity</h6>
-            <p>Solid</p>
-            <h6>Time Management</h6>
-            <p>Good</p>
-            <h6>Self-Learner</h6>
-            <p>Great</p>
-            <h6>Flexibility</h6>
-            <p>Strong</p>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>CSS</h4>
+            <small className="text-light">Experienced</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>JavaScript</h4>
+            <small className="text-light">Intermediate</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Bootstrap</h4>
+            <small className="text-light">Experienced</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>React</h4>
+            <small className="text-light">Intermediate</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Git & Github</h4>
+            <small className="text-light">Experienced</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Figma</h4>
+            <small className="text-light">Intermediate</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Ajax</h4>
+            <small className="text-light">Experienced</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>SEO management</h4>
+            <small className="text-light">Intermediate</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Responsive Designs</h4>
+            <small className="text-light">Experienced</small>
+            </div>
+            </div>
+            </div>
+            </div>
+        <div className="soft-skills">
+            <h2>Soft Skills</h2>
+            <div className="experience__content2">
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Team Work</h4>
+            <small className="text-light">Strong</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Problem Solving</h4>
+            <small className="text-light">Good</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Creativity</h4>
+            <small className="text-light">Solid</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Time Management</h4>
+            <small className="text-light">Good</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Self-Learner</h4>
+            <small className="text-light">Great</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Flexibility</h4>
+            <small className="text-light">Strong</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Communication Skills</h4>
+            <small className="text-light">Strong</small>
+            </div>
+            </div>
+            <div className="experience__details">
+            <BsPatchCheckFill/>
+            <div className="wrapper">
+            <h4>Accountability</h4>
+            <small className="text-light">Strong</small>
+            </div>
+            </div>
+            </div>
             </div>
             </div>
             </div>
