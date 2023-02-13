e437592acb178d344e458b1fa1c1f16dd5704f2b
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-02-13 22:55:18 +0100

    Completed Footer Section
    Added Social Handles to the Footer Section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import './Footer.css'
+import './Footer.css';
+import { BsLinkedin, BsGithub ,BsInstagram} from 'react-icons/bs';
 
 export default function Footer(){
     return(
@@ -12,7 +13,11 @@ export default function Footer(){
                 <li><a href="#portfolio" title="Portfolio page">Portfolio</a></li>
                 <li><a href="#contact" title="Contact page">Contact</a></li>
             </ul>
-            <div className="handles"></div>
+            <div className="handles">
+            <a href="https://www.linkedin.com/in/sylvester-promise-7492a5247/" target="_blank" rel="noopener noreferrer"><BsLinkedin className="footer-icon"/></a> 
+           <a href="https://github.com/Program-doctor" target="_blank" rel="noopener noreferrer"><BsGithub className="footer-icon"/></a> 
+           <a href="https://www.instagram.com/promise.syl/" target="_blank" rel="noopener noreferrer"><BsInstagram className="footer-icon"/></a>
+            </div>
             <p>Built with ReactJs and Netlify forms</p>
             <small>
                 &copy; Sylvester Promise 2023. All rights reserved</small>
