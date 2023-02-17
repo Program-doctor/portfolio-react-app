7d859e8c0e3ae5a1d15874be76a7cfd26c18c0c8
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-02-17 12:40:10 +0100

    update

--- src/components/contact/success.js
+++ src/components/contact/success.js
@@ -9,7 +9,7 @@ import './Success.css'
         <div id="card">
             <div className="success">
             <h3>Thank you for leaving a comment. I hope to see you more often and do have a wonderful time <span role="img" aria-label="smiling">😄</span></h3>
-            <a href="https://imaginative-pastelito-552212.netlify.app/?"><BsFillArrowLeftCircleFill/>Back to the site</a>
+            <a href="https://imaginative-pastelito-552212.netlify.app/#contact"><BsFillArrowLeftCircleFill/>Back to the site</a>
             </div>
         </div>
     );
